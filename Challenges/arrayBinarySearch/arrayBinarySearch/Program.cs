e71dc407c316170094a2bb3b08f026e6bbef67ff
trecain <developedbycain@yahoo.com>
2018-10-11 12:49:07 -0500

    wrote my implementation of my whiteboard with a few changes

---
+++ Challenges/arrayBinarySearch/arrayBinarySearch/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace arrayBinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testArray = { 4, 8, 15, 16, 23, 42 };
+            int[] testArrayTwo = { 11, 22, 33, 44, 55, 66, 77 };
+            Console.WriteLine(BinarySearch(testArray, 15));
+            Console.WriteLine(BinarySearch(testArrayTwo, 15));
+        }
+
+        public static int BinarySearch(int[] sortedArray, int searchKey)
+        {
+            int min = 0;
+            int max = sortedArray.Length;
+            
+            while (min <= max)
+            {
+                int middle = (min + max) / 2;
+                if (sortedArray[middle] < searchKey )
+                {
+                    min = middle + 1;
+                }
+                else if (sortedArray[middle] > searchKey)
+                {
+                    max = middle - 1;
+                }
+                else
+                {
+                    return middle;
+                }
+            }
+            return -1;
+        }
+    }
+}
+
