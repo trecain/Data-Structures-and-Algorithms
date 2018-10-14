50bdc5d8413433dba3c9a9d882e020751e086569
trecain <developedbycain@yahoo.com>
2018-10-14 18:08:36 -0500

    trash

--- Challenges/fibonacci/fibonacci/Program.cs
+++ Challenges/fibonacci/fibonacci/Program.cs
@@ -6,7 +6,30 @@ namespace fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the Fibonacci Sequence.");
+        }
+
+        public static int FibonacciSequence(int num)
+        {
+            int result = 1, numOne = 0, numTwo = 1;
+            if (num == 0)
+            {
+                return num;
+            }
+            else if (num <= 2)
+            {
+                return 1;
+            }
+            else
+            {
+                for (int i = 0; i < num; i++)
+                {
+                    numOne = numTwo;
+                    numTwo = result;
+                    result = numOne + numTwo;
+                }
+            }
+            return numOne;
         }
     }
 }
