6c1cf51b0637a99156ba771ab975c8d3ceb0daef
trecain <developedbycain@yahoo.com>
2018-10-22 00:46:31 -0500

    two of my testing passing others failing

--- Challenges/ReversedLL/ReversedLLTest/UnitTest1.cs
+++ Challenges/ReversedLL/ReversedLLTest/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System;
 using Xunit;
 using Day06_LinkedList.Classes;
-using 
+using ReversedLL;
 
 namespace ReversedLLTest
 {
@@ -13,9 +13,39 @@ namespace ReversedLLTest
             LList linkedList = new LList(new Node(3));
             linkedList.Add(new Node(9));
             linkedList.Add(new Node(4));
-            linkedList.Add(new Node(2));
 
-            Assert("",)
+            Assert.Equal("3 --> 9 --> 4 --> NULL\n", Program.ReverseLL(linkedList).Print());
+        }
+
+
+        [Fact]
+        public void CanReverseLLSixNumbers()
+        {
+            LList linkedList = new LList(new Node(3));
+            linkedList.Add(new Node(9));
+            linkedList.Add(new Node(4));
+            linkedList.Add(new Node(9));
+            linkedList.Add(new Node(4));
+            linkedList.Add(new Node(4));
+
+            Assert.Equal("3 --> 9 --> 4 --> 9 --> 4 --> 4 --> NULL\n", Program.ReverseLL(linkedList).Print());
+        }
+
+        [Fact]
+        public void CanReverseLLTenNumbers()
+        {
+            LList linkedList = new LList(new Node(3));
+            linkedList.Add(new Node(9));
+            linkedList.Add(new Node(4));
+            linkedList.Add(new Node(9));
+            linkedList.Add(new Node(4));
+            linkedList.Add(new Node(4));
+            linkedList.Add(new Node(4));
+            linkedList.Add(new Node(9));
+            linkedList.Add(new Node(4));
+            linkedList.Add(new Node(4));
+
+            Assert.Equal("9 --> 4 --> 4 --> 4 --> 9 --> 4 --> 4 --> NULL\n", Program.ReverseLL(linkedList).Print());
         }
     }
 }
