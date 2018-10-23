e48cd56d53b627f3359264186634d48dc625e5aa
trecain <developedbycain@yahoo.com>
2018-10-23 00:00:20 -0500

    added the new project

---
+++ Challenges/queue_with_stacks/queue_with_stacks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace queue_with_stacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
