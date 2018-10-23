8e2cc52376037567affbd78dac6e0c2b470642cd
trecain <developedbycain@yahoo.com>
2018-10-23 12:50:54 -0500

    start from scratch

--- Challenges/queue_with_stacks/queue_with_stacks/Program.cs
+++ Challenges/queue_with_stacks/queue_with_stacks/Program.cs
@@ -8,17 +8,10 @@ namespace queue_with_stacks
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Queue q = new Queue(new Node(10));
-            q.addsNodeToEndOfTheQueue(new Node(15));
-            q.addsNodeToEndOfTheQueue(new Node(20));
-            q.addsNodeToEndOfTheQueue(new Node(5));
+            Console.WriteLine("Queue with Two Stacks");
 
-            Console.WriteLine("Last node added");
-            Console.WriteLine(q.RemovesNodeFromFrontOfTheQueue().Value);
-            Console.WriteLine(q.RemovesNodeFromFrontOfTheQueue().Value);
-            Console.WriteLine(q.RemovesNodeFromFrontOfTheQueue().Value);
-            Console.WriteLine(q.RemovesNodeFromFrontOfTheQueue().Value);
+            Stack stackOne = new Stack(new Node(3));
+            Stack stackTwo = new Stack(new Node(1));
 
         }
     }
