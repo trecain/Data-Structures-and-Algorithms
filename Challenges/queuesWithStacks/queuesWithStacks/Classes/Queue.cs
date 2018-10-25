dad19be5a6f957d5fee735f60c699492ff284d36
trecain <developedbycain@yahoo.com>
2018-10-25 12:44:34 -0500

    added a Que class with comments

---
+++ Challenges/queuesWithStacks/queuesWithStacks/Classes/Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StackAndQueue.Classes;
+using Day06_LinkedList.Classes;
+
+namespace queuesWithStacks.Classes
+{
+    class Queue
+    {
+        /// <summary>
+        /// Sets the properties to hold the two stacks for the queue
+        /// </summary>
+        private Stack stackOne { get; set; }
+        private Stack stackTwo { get; set; }
+
+
+        /// <summary>
+        /// Initializes the two stacks
+        /// </summary>
+        /// <param name="node">Node needed to initialize the stack</param>
+        public Queue(Node node)
+        {
+            stackOne = new Stack(node);
+            stackTwo = new Stack(node);
+        }
+
+        /// <summary>
+        /// returns the top node on the stack
+        /// </summary>
+        /// <returns></returns>
+        public Node Peek()
+        {
+            return stackOne.View();
+        }
+
+
+        /// <summary>
+        /// Enqueues a specified node 
+        /// </summary>
+        /// <param name="node"></param>
+        public void Enqueue(Node node)
+        {
+            while (stackOne.TopNode != null)
+            {
+                stackTwo.Push(stackOne.Pop());
+            }
+
+            while (stackTwo.TopNode != null)
+            {
+                stackOne.Push(stackTwo.Pop());
+            }
+        }
+
+        public Node Dequeue()
+        {
+            if (stackOne.TopNode != null)
+            {
+                try
+                {
+                    return stackOne.Pop();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+            throw new NullReferenceException();
+        }
+
+    }
+}
---
+++ Challenges/queuesWithStacks/queuesWithStacks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace queuesWithStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lab 11: Queue with stacks");
+        }
+
+
+    }
+}
