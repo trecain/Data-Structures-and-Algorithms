31daa02f32b8757401b5d0c1953dfa4e9af58824
trecain <developedbycain@yahoo.com>
2018-11-01 12:14:10 -0500

    added project to file

---
+++ Challenges/multi-bracket-validation/multi-bracket-validation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multi_bracket_validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coding Challenge: Multi-Bracket Validation");
+        }
+    }
+}
