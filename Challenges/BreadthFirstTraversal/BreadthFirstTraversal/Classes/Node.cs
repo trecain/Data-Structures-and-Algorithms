f0864413b646dd9a03ee37ce023b767ecc35afa3
trecain <developedbycain@yahoo.com>
2018-11-02 00:13:05 -0500

    added

---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Classes/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadthFirstTraversal.Classes
+{
+    public class Node
+    {
+        /// <summary>
+        /// Setting the properties for the node, left and right
+        /// </summary>
+        public int Data { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+
+        /// <summary>
+        /// Constructor for node, sets left and right node to null
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(int value)
+        {
+            Data = value;
+        }
+    }
+}
---
+++ Challenges/BreadthFirstTraversal/BreadthFirstTraversal/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BreadthFirstTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coding Challenge: Breadth First Traversal");
+        }
+
+
+    }
+}
